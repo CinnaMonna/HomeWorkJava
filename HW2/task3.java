d2f2d1cd6d877515b92e69abdbdeaf8c6c78ab82
Natalia <nivanova86@bk.ru>
2023-02-16 06:10:03 +0300

    add task3

--- HW2/task3.java
+++ HW2/task3.java
@@ -3,6 +3,33 @@
 
 package HW2;
 
+import java.lang.StringBuilder;
+
 public class task3 {
+    public static void main(String[] args) {
+        String a = "potop";
+        String b = "шалаш";
+        String c = "A2332A";
+        String d = "not_pal";
+        System.out.print(a + " - ");
+        System.out.println(isPalindrom(a));
+        System.out.print(b + " - ");
+        System.out.println(isPalindrom(b)); 
+        System.out.print(c + " - ");
+        System.out.println(isPalindrom(c)); 
+        System.out.print(d + " - ");
+        System.out.println(isPalindrom(d));     
+    }
+
+    static boolean isPalindrom(String str) {
+        StringBuilder strB = new StringBuilder(str);
+        int len = strB.length();
+        String half2 = strB.substring(len - len / 2);
+        strB.setLength(len / 2);
+        String half1 = strB.reverse().toString();
+       
+        return half1.equals(half2);
+        
+    }
     
 }
